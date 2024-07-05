d64761277df47ac3514b9a472f70041bed976dac
Simon Kelly <skelly@dimagi.com>
2024-07-05 14:27:54 +0200

    ruff format and pre-commit

--- tests/unit/test_ocs_deploy_stack.py
+++ tests/unit/test_ocs_deploy_stack.py
@@ -3,12 +3,14 @@ import aws_cdk.assertions as assertions
 
 from ocs_deploy.ocs_deploy_stack import OcsDeployStack
 
+
 # example tests. To run these tests, uncomment this file along with the example
 # resource in ocs_deploy/ocs_deploy_stack.py
 def test_sqs_queue_created():
     app = core.App()
     stack = OcsDeployStack(app, "ocs-deploy")
-    template = assertions.Template.from_stack(stack)
+    template = assertions.Template.from_stack(stack)  # noqa: F841
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
