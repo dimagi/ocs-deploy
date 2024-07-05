1cdfb592af37a0a37868a9a669c0f133262def3d
Simon Kelly <skelly@dimagi.com>
2024-07-05 15:05:31 +0200

    use env

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 import aws_cdk as cdk
 from dotenv import load_dotenv
@@ -11,16 +12,9 @@ app = cdk.App()
 OcsDeployStack(
     app,
     "OcsDeployStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    # env=cdk.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    env=cdk.Environment(
+        account=os.getenv("CDK_ACCOUNT"), region=os.getenv("CDK_REGION")
+    ),
 )
 
 app.synth()
