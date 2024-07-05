cfc035fc6898361b802410e76b00804c66666137
Simon Kelly <skelly@dimagi.com>
2024-07-05 15:09:46 +0200

    better construct ID

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ from ocs_deploy.ocs_deploy_stack import OcsDeployStack
 
 load_dotenv(".env")
 
+app_name = os.getenv("APP_NAME", "open-chat-studio")
+environment = os.getenv("ENVIRONMENT", "dev")
+cdk_region = os.getenv("CDK_REGION")
+
 app = cdk.App()
 OcsDeployStack(
     app,
-    "OcsDeployStack",
-    env=cdk.Environment(
-        account=os.getenv("CDK_ACCOUNT"), region=os.getenv("CDK_REGION")
-    ),
+    f"{app_name}-{environment}-{cdk_region}-OcsDeployStack`",
+    env=cdk.Environment(account=os.getenv("CDK_ACCOUNT"), region=cdk_region),
 )
 
 app.synth()
