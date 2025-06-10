8c090b9978965b960abfd8924fb4a47f86665241
Simon Kelly <skelly@dimagi.com>
2025-06-10 17:45:20 +0200

    comment out security features for now

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ from ocs_deploy.redis import RedisStack
 from ocs_deploy.s3 import S3Stack
 from ocs_deploy.vpc import VpcStack
 from ocs_deploy.waf import WAFStack
-from ocs_deploy.guardDuty import GuardDutyStack
-from ocs_deploy.securityhub import SecurityHubStack
-from ocs_deploy.detective import DetectiveStack
 
 app = cdk.App()
 env = app.node.try_get_context("ocs_env")
@@ -43,11 +40,12 @@ ocs_services = FargateStack(
 waf_stack = WAFStack(app, config, ocs_services.load_balancer_arn)
 waf_stack.add_dependency(ocs_services)
 
-guardduty_stack = GuardDutyStack(app, config)
-
-securityhub_stack = SecurityHubStack(app, config)
-
-detectiveStack = DetectiveStack(app, config)
+# these aren't deployed to prod yet
+# guardduty_stack = GuardDutyStack(app, config)
+#
+# securityhub_stack = SecurityHubStack(app, config)
+#
+# detectiveStack = DetectiveStack(app, config)
 
 ocs_services.add_dependency(vpc_stack)
 ocs_services.add_dependency(ecr_stack)
